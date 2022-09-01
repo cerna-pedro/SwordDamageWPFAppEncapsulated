6abf6f0a772e977a4c85b770ec30d1b6fc9a943f
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-08-31 19:33:30 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SwordDamageWPFAppEncapsulated
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage = new SwordDamage((random.Next(1,7)+random.Next(1,7)+random.Next(1,7)));
+            DisplayDamage();
+        }
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamage();
+        }
+        void DisplayDamage()
+        {
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+        private void Flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = 2;
+            DisplayDamage();
+
+        }
+        private void Flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = 0;
+            DisplayDamage();
+        }
+        private void Magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = 1.75M;
+            DisplayDamage();
+        }
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = 1M;
+            DisplayDamage();
+        }
+    }
+}
---
+++ SwordDamage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordDamageWPFAppEncapsulated
+{
+    internal class SwordDamage
+    {
+        public SwordDamage(int roll)
+        {
+            this.roll = roll;
+            CalculateDamage();
+        }
+
+        public const int BASE_DAMAGE = 3;
+
+        private int roll;
+        private int flaming = 0;
+        private decimal magic=1M;
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        public int Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+        public decimal Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        public int Damage { get; private set; }
+
+        private void CalculateDamage()
+        {
+            Damage = (int)(Roll * Magic) + BASE_DAMAGE + Flaming;
+        }
+    }
+}
